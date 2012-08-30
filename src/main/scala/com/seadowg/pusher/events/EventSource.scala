3937caa1372de6ffc0ef674eeb4d8690945511b4
Callum Stott <oetzi101@gmail.com>
2012-08-31 00:37:44 +0100

    Remove EventSource
    Processing should be kept to the EventProcessor. The event package
    should simply serve as an API for creating EventStreams.

--- src/main/scala/com/seadowg/pusher/events/EventSource.scala
+++
@@ -1,9 +0,0 @@
-package com.seadowg.pusher.events
-
-import com.seadowg.pusher.runtime.EventProcessor
-
-class EventSource[T] extends EventStream[T] {
-  def occur(value: T) {
-    EventProcessor.process(this, value)
-  }
-}
