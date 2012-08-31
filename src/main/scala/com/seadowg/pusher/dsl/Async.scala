892738abe2eba5b20f6e7bfe76cb01196568631a
Callum Stott <oetzi101@gmail.com>
2012-08-31 11:52:35 +0100

    Actually implement async this time

--- src/main/scala/com/seadowg/pusher/dsl/Async.scala
+++ src/main/scala/com/seadowg/pusher/dsl/Async.scala
@@ -2,14 +2,16 @@ package com.seadowg.pusher.dsl
 
 import scala.actors.Actor.actor
 import com.seadowg.pusher.events.Event
+import com.seadowg.pusher.events.EventStream
 import com.seadowg.pusher.runtime.EventProcessor
 
 object Async {
-  def async[T](work: => T) {
+  def async[T](work: => T): EventStream[T] = {
+    val event = new EventStream[T]
     actor {
-      new Event[T] {
-        EventProcessor.process(this, work)
-      }
+      EventProcessor.process(event, work)
     }
+    
+    event
   }
 }
