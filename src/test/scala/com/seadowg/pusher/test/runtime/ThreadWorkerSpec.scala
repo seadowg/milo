7abf4b2bee81905da2bc6afe31738422dafe6e99
Callum Stott <oetzi101@gmail.com>
2013-01-13 20:12:37 -0800

    Correct name of spec file

---
+++ src/test/scala/com/seadowg/pusher/test/runtime/ThreadWorkerSpec.scala
@@ -0,0 +1,26 @@
+package com.seadowg.milo.test.runtime
+
+import org.specs2.mutable._
+import org.specs2.mock._ 
+import com.seadowg.milo.test.helpers.AsyncHelpers._
+import com.seadowg.milo.runtime._
+import com.seadowg.milo.events.Event
+
+class ThreadWorkerSpec extends Specification with Mockito {
+  "ThreadWorker" should {
+		"execute work it is sent" in {
+			val worker = new ThreadWorker()
+			var executed = false
+			
+			worker.spawn()
+			
+			worker.send(() => executed = true)
+			waitUntil(() => executed) mustEqual true
+			executed = false
+			
+			worker.send(() => executed = true)
+			waitUntil(() => executed) mustEqual true
+		}
+	}
+}
+
