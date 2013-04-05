d0be6a955a7b50cfd13dcc4e7f268d823ef99f6f
Callum Stott <oetzi101@gmail.com>
2013-04-04 22:55:12 -0700

    Rewrite EventStream specs

--- src/test/scala/com/seadowg/milo/test/events/EventStreamSpec.scala
+++ src/test/scala/com/seadowg/milo/test/events/EventStreamSpec.scala
@@ -1,47 +1,52 @@
 package com.seadowg.milo.test.events
 
 import org.specs2.mutable._
+import org.specs2.mock._
 import com.seadowg.milo.events._
 
 
-class EventStreamSpec extends Specification {
+class EventStreamSpec extends Specification with Mockito {
   "EventStream" should {
+    "bind(func)" should {
+      "pass the function to the the stream's event.bind" in {
+        val mockEvent = mock[Event[Int]]
+        val stream = new EventStream(mockEvent)
+        val func: Int => Unit = { i => i }
+
+        stream.bind(func)
+        there was one(mockEvent).bind(func)
+      }
+    }
+
+    "map(func)" should {
+      "return a stream with the function applied to every element of the original" in {
+        var sent = 0
+        val stream = new EventStream[Int]
+        stream.map(value => value * 2).event.bind(value => sent = value)
+        stream.event.trigger(1)
+
+        sent mustEqual 2
+      }
+    }
+
+    "filter(func)" should {
+      "return a stream containing only the elements that pass the filter" in {
+        var sent = 0
+        val stream = new EventStream[Int]
+        stream.filter(value => value < 2).event.bind(value => sent = value)
+        stream.event.trigger(1)
+        stream.event.trigger(2)
+
+        sent mustEqual 1
+      }
+    }
+
     "always have an event" in {
       val stream1 = new EventStream(new Event[Int])
       stream1.event must_!= null
-      
+
       val stream2 = new EventStream()
       stream2.event must_!= null
-    }
-    
-    "should pass binded functions to its Event" in {
-      var executed = false
-      val stream = new EventStream[Boolean]
-      stream.bind {
-        value => executed = value
-      }
-      
-      stream.event.trigger(true)
-      executed must beTrue
-    }
-    
-    "sends correct values to mapped EventStream events" in {
-      var sent = 0
-      val stream = new EventStream[Int]
-      stream.map(value => value * 2).event.bind(value => sent = value)
-      stream.event.trigger(1)
-      
-      sent mustEqual 2
-    }
-    
-    "sends only passing values to a filtered EventStream event" in {
-      var sent = 0
-      val stream = new EventStream[Int]
-      stream.filter(value => value > 1).event.bind(value => sent += value)
-      stream.event.trigger(1)
-      stream.event.trigger(2)
-      
-      sent mustEqual 2
     }
   }
 }
